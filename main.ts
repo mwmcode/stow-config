032d70ae2096626ffd87eebcb86be02f83809d80
mch <omitted@email.com>
2025-01-06 22:46:47 -0500

    added args, test step

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-import { stowConfig } from './src/mod.ts';
+import { Command } from '@cliffy/command';
+import { stowConfig } from '@/src/mod.ts';
 
 if (!import.meta.main) {
   console.error('This script must be run in standalone mode.');
   Deno.exit(1);
 }
 
-stowConfig();
+const { options } = await new Command()
+  .name('stow')
+  .description('Run it without argument options to start')
+  .option('-v, --version', 'Display version')
+  .action(async () => {
+    const module = await import('./deno.json', {
+      with: { type: 'json' },
+    });
+    console.log(module.default.version);
+  })
+  .parse(Deno.args);
+
+if (Object.keys(options).length === 0) {
+  stowConfig();
+}
