44cbb85d37eb047a5495fcec86bad2243ea3aca9
mch <omitted@email.com>
2025-01-06 22:57:11 -0500

    updated test to use relative paths

--- test/consts.ts
+++ test/consts.ts
@@ -1,9 +1,12 @@
-import { join } from '@std/path';
-
 export const TEST_DENO_ARGS = ['-R', '-W', '--allow-env=SNAPSHOT_TEST_NAME'];
 
-export const SRC_DIR = join(Deno.cwd(), 'test/data/config-src');
+export const SRC_DIR = 'test/data/config-src';
 
-export const DEST_DIR = join(Deno.cwd(), 'test/data/config-dest');
+export const DEST_DIR = 'test/data/config-dest';
 
-export const CONFIGS = ['.prettierrc.js', 'git/config', '.zshrc', 'starship.toml'];
+export const CONFIGS = [
+  '.prettierrc.js',
+  'git/config',
+  '.zshrc',
+  'starship.toml',
+];
