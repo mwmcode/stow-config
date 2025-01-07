b0d42a48677c875ac0ecd0d2914a119221914a84
mch <omitted@email.com>
2025-01-07 00:25:35 -0500

    fixed ts issue

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Command } from '@cliffy/command';
 import { stowConfig } from '@/src/mod.ts';
+import { logger } from '@/src/logger.ts';
 
 if (!import.meta.main) {
   console.error('This script must be run in standalone mode.');
@@ -10,12 +11,10 @@ const { options } = await new Command()
   .name('stow')
   .description('Run it without argument options to start')
   .option('-v, --version', 'Display version')
-  .action(async (options: { v: boolean; version: boolean }) => {
-    if (!options.v && !options.version) return;
-    const module = await import('./deno.json', {
-      with: { type: 'json' },
-    });
-    console.log(module.default.version);
+  .action(async options => {
+    if (!options.version) return;
+    const module = await import('./deno.json', { with: { type: 'json' } });
+    console.log('jsr:@mcha/stow-config', logger.bold(module.default.version));
   })
   .parse(Deno.args);
 
